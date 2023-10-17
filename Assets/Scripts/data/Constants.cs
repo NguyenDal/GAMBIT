4512546b4e690174740ca4bd4d6d2964e506cef6
Lydia Verboom <pr253146@dal.ca>
2023-10-17 14:16:25 -0400

    Fixed merge conflicts

--- Assets/Scripts/data/Constants.cs
+++ Assets/Scripts/data/Constants.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿namespace data
+namespace data
 {
     public static class Constants
     {
@@ -15,21 +14,3 @@
         public const int City2D = 5;
     }
 }
-=======
-﻿namespace data
-{
-    public static class Constants
-    {
-        //This is where all of the output values are stored.
-        public const string OutputDirectory = "Assets/Outputfiles~";
-
-        //Scene numbers.
-        public const int LoadScene = 0;
-        public const int Jungle2D = 4;
-        public const int CityTerrain = 2;
-        public const int JungleTerrain = 3;
-        public const int LoadingScreen = 1;
-        public const int City2D = 5;
-    }
-}
->>>>>>> 68e09903d6f70fa2597b7acf45c703816e62bb39
--- Assets/Scripts/twoDSystem/SpriteController.cs
+++ Assets/Scripts/twoDSystem/SpriteController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using trial;
@@ -37,43 +36,3 @@ namespace twoDSystem
 
 
 }
-=======
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
-using trial;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using wallSystem;
-using DS = data.DataSingleton;
-using G = wallSystem.GenerateWall;
-using E = main.Loader;
-using C = data.Constants;
-namespace twoDSystem
-{
-	public class SpriteController : MonoBehaviour
-	{
-		// Update is called once per frame
-		private void Update () {
-			//This calculates the current amount of rotation frame rate independent
-			var rotation = Input.GetAxis("Horizontal") * DS.GetData().CharacterData.RotationSpeed * Time.deltaTime;
-
-			//This calculates the forward speed frame rate independent
-			var moveDirection = new Vector3(0, Input.GetAxis("Vertical"), 0);
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= DS.GetData().CharacterData.MovementSpeed;
-
-			//Here is the movement system
-			const double tolerance = 0.0001;
-
-			//we move iff rotation is 0
-			if (Math.Abs(Mathf.Abs(rotation)) < tolerance)
-				GetComponent<CharacterController>().Move(moveDirection * Time.deltaTime);
-
-			transform.Rotate(0, 0, -rotation);
-		}
-	}
-
-
-}
->>>>>>> 68e09903d6f70fa2597b7acf45c703816e62bb39
