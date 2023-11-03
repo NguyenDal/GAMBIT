59c270ef8f5fe9b09ac2422344c52d9fd3a76ff7
Lydia Verboom <pr253146@dal.ca>
2023-11-03 15:50:06 -0400

    Updated .gitignore

--- Assets/Scripts/Input/ActionObject.cs
+++ Assets/Scripts/Input/ActionObject.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CreateAction", order = 1)]
 public class ActionObject : ScriptableObject
@@ -9,5 +10,11 @@ public class ActionObject : ScriptableObject
     public string actionName;
     public string keyName;
 
-    public Action action;
+    public UnityEvent eventName;
+
+    void checkForInput()
+    {
+       // if (Input.GetButton)
+    }
+    
 }
