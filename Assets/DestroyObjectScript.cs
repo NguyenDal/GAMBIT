786b9f9a2997f838137a131759890071707eb495
Ariya Moez Dhukka <ariyadhukka786@gmail.com>
2024-02-01 21:03:16 -0400

    Done writing the script for destroying the barriers

---
+++ Assets/DestroyObjectScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectScript : MonoBehaviour
+{
+    public GameObject wall;    //gameobj1
+    public GameObject player;   //gameobj2
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (wall != null) {
+            if (Input.GetKey(KeyCode.UpArrow) && (player.transform.position.x <= wall.transform.position.x + 1) && 
+            (player.transform.position.x >= wall.transform.position.x - 1) && (player.transform.position.y <= wall.transform.position.y + 1) &&
+            (player.transform.position.y >= wall.transform.position.y - 1)) {
+                wall.SetActive(false);
+            }
+        }
+    }
+    // void OnTriggerEnter(Collider other) {
+    //     Debug.Log("Collided!");
+    //     if (other.gameObject.CompareTag("Wall")) {
+            
+    //     }  
+    // }
+}
