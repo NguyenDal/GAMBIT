a6774490c0d4bebc4e8c5953b0736142f58638bd
Charles <ch253061@dal.ca>
2024-02-29 14:25:31 -0400

    Replaced 2D Objects with canvas + buttons
    Replaced the 2D Flickering Objects on the HUD to a button in a canvas as a UI layer. Renamed FlickerTest script to ButtonFlickerScript

---
+++ Assets/Scripts/ButtonFlickerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFlickerScript : MonoBehaviour
+{
+    // Two colors to indicate the refresh difference
+    public Color startColor = Color.white;
+    public Color endColor = Color.grey;
+
+    // Range slider for the 'inspector' tool in unity to adjust speed
+    [Range (0, 10)]
+    public float speed = 1;
+
+    // Initialize Renderer
+    Image img;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        img.material.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time * speed, 1));
+    }
+
+
+}
