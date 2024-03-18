e05ac87aa9251f6ca9c2a5ff535144545a567c95
Abhishek Selvakumar <ab509231@dal.ca>
2024-03-18 17:41:22 -0300

    The configuration file works now

--- Assets/Scripts/OnlyFlickerWhenNear.cs
+++ Assets/Scripts/OnlyFlickerWhenNear.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class onlyFlickerWhenNear : MonoBehaviour
+public class OnlyFlickerWhenNear : MonoBehaviour
 {
 
     private GameObject player;
