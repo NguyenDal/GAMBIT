3295c37682306c6a1370a9c37edc46ab5d7ca12f
Gavin Sharma <gv450463@dal.ca>
2024-07-03 05:47:16 -0300

    feat: updated the respawn script to include a method for setting checkpoint

--- Assets/Scripts/wallSystem/respawn.cs
+++ Assets/Scripts/wallSystem/respawn.cs
@@ -2,33 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine.UIElements;
-
 public class respawn : MonoBehaviour
 {
     private GameObject participent;
     private GameObject trans;
     private Transform respawnLocation;
+    private Vector3 checkpointPosition;
+    private bool checkpointSet = false;
 
     private GameObject[] walls;
 
-    void start(){
+    void start()
+    {
         walls = GameObject.FindGameObjectsWithTag("Cube");
     }
-    public void Respawn(){
-        participent = GameObject.FindGameObjectWithTag("Player");
-        trans = GameObject.FindGameObjectWithTag("Respawn");
-        respawnLocation = trans.transform;
-        participent.transform.position = respawnLocation.position;
-    }
 
-    void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.name.Equals("Participant")){
-            participent = GameObject.FindGameObjectWithTag("Player");
+    public void Respawn()
+    {
+        participent = GameObject.FindGameObjectWithTag("Player");
+        if (checkpointSet)
+        {
+            participent.transform.position = checkpointPosition;
+        }
+        else
+        {
             trans = GameObject.FindGameObjectWithTag("Respawn");
             respawnLocation = trans.transform;
             participent.transform.position = respawnLocation.position;
         }
     }
-    
+
+    public void SetCheckpoint(Vector3 position)
+    {
+        checkpointPosition = position;
+        checkpointSet = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Participant"))
+        {
+            participent = GameObject.FindGameObjectWithTag("Player");
+            if (checkpointSet)
+            {
+                participent.transform.position = checkpointPosition;
+            }
+            else
+            {
+                trans = GameObject.FindGameObjectWithTag("Respawn");
+                respawnLocation = trans.transform;
+                participent.transform.position = respawnLocation.position;
+            }
+        }
+    }
 }
