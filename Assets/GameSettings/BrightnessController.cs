d4a622a26217612617c0e844a2907da9fd9732b5
pdoshi <pr882442@dal.ca>
2024-07-04 15:00:27 -0300

    changed the brightnessmanager to adjust default brightness

--- Assets/GameSettings/BrightnessController.cs
+++ Assets/GameSettings/BrightnessController.cs
@@ -4,27 +4,35 @@ using UnityEngine.UI;
 public class BrightnessController : MonoBehaviour
 {
     public Slider brightnessSlider;
-    public Light DirectionalLight;
+    public Light directionalLight;
 
-    public void Start()
+    void Start()
     {
-        float defaultBrightness = 2f;
-
-        // Save the default brightness value to PlayerPrefs if it hasn't been set before
+        float defaultBrightness = 2.5f;
         if (!PlayerPrefs.HasKey("Brightness"))
         {
             PlayerPrefs.SetFloat("Brightness", defaultBrightness);
+            brightnessSlider.value = defaultBrightness;
         }
-
         float savedBrightness = PlayerPrefs.GetFloat("Brightness", defaultBrightness);
+
         brightnessSlider.value = savedBrightness;
-        DirectionalLight.intensity = Mathf.PingPong(Time.time, savedBrightness);
+        UpdateLightBrightness(savedBrightness);
+
         brightnessSlider.onValueChanged.AddListener(OnBrightnessChange);
     }
 
     void OnBrightnessChange(float value)
     {
-        DirectionalLight.intensity = value;
-        PlayerPrefs.SetFloat("Brightness", value);  // Save the current brightness value
+        UpdateLightBrightness(value);
+        PlayerPrefs.SetFloat("Brightness", value);
+    }
+
+    void UpdateLightBrightness(float brightness)
+    {
+        if (directionalLight != null)
+        {
+            directionalLight.intensity = Mathf.PingPong(Time.time, brightness); ;
+        }
     }
 }
