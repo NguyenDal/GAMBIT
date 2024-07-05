4a42a0c3f474cebdbab1d852e5ef2a05fc86150e
Corex <lcm212243@gmail.com>
2024-07-05 12:12:46 -0400

    Fixed respawn logic to the player, added death animation triggers, and resaved enemy prefabs

--- Assets/Scripts/wallSystem/respawn.cs
+++ Assets/Scripts/wallSystem/respawn.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +12,8 @@ public class respawn : MonoBehaviour
 
     private GameObject[] walls;
 
+    public Animator animator;
+
     void start(){
         walls = GameObject.FindGameObjectsWithTag("Cube");
     }
@@ -21,14 +23,33 @@ public class respawn : MonoBehaviour
         respawnLocation = trans.transform;
         participent.transform.position = respawnLocation.position;
     }
-
     void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.name.Equals("Participant")){
-            participent = GameObject.FindGameObjectWithTag("Player");
-            trans = GameObject.FindGameObjectWithTag("Respawn");
-            respawnLocation = trans.transform;
-            participent.transform.position = respawnLocation.position;
+        if (collision.gameObject.name.Equals("Participant")){
+            StartCoroutine(WaitForDeathAnimation());
         }
     }
-    
+
+    void OnTriggerEnter(Collider other){
+        if (other.tag != "EnemyAggroRange"){
+            StartCoroutine(WaitForDeathAnimation());
+        }
+    }
+
+    IEnumerator WaitForDeathAnimation(){
+        // Play the death animation
+        animator.SetBool("IsDead", true);
+
+        // Wait for the death animation to finish
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        while (!(stateInfo.IsName("Death") && stateInfo.normalizedTime >= 1.0f)){
+            yield return null;
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        }
+
+        // Reset the "IsDead" parameter
+        animator.SetBool("IsDead", false);
+
+        // Respawn the player
+        Respawn();
+    }
 }
