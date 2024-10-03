9eff5480a9b5e45e5726214d8351370966c0ffe5
Ryan O'Toole <ry404033@dal.ca>
2024-10-03 18:55:22 -0300

    reverting to fa4b75656f39705d32bbf52def8b5acc424641a6

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int starsCollected = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+}
